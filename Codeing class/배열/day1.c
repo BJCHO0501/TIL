568b8c243a297a8c86f209eaf040d428e1eb9b48
조병진 <bjcho@jobyeongjin-ui-MacBookAir.local>
2022-04-19 12:20:02 +0900

    프밍 배열 추가

---
+++ Codeing class/배열/day1.c
@@ -0,0 +1,71 @@
+//
+//  main.c
+//  codeup
+//
+//  Created by 조병진 on 2022/04/03.
+//
+
+//배열
+#include <stdio.h>
+
+int main() {
+    //    int num, min = 1001;
+    //
+    //    while (1) {
+    //        scanf("%d", &num);
+    //        if (num < 1000){
+    //            if (num < min){
+    //                min = num;
+    //            }
+    //        }
+    //        else{
+    //            break;
+    //        }
+    //    }
+    //
+    //    printf("%d\n", min);
+        
+    //    int num[11], max = 0;
+    //
+    //
+    //    for (int i = 0; i < 10; i ++){
+    //        scanf("%d", &num[i]);
+    //    }
+    //
+    //    for (int j = 9; j >= 0; j--){
+    //        if (num[j] > max){
+    //            max = num[j];
+    //        }
+    //    }
+    //
+    //    printf("%d\n", max);
+        
+    //    int num[101], i = 0;
+    //
+    //    for (i = 0; ; i++){
+    //        scanf("%d", &num[i]);
+    //        if (num[i] == 0){
+    //            break;
+    //        }
+    //    }
+    //
+    //
+    //    for (i = i - 1; i >= 0 ; i--){
+    //        printf("%d \n", num[i]);
+    //    }
+        
+        
+    //    int num[11], sum = 0;
+    //
+    //    for (int i = 0; i < 10; i++){
+    //        scanf("%d", &num[i]);
+    //    }
+    //
+    //    for (int j = 0; j < 10; j++){
+    //        sum += num[j];
+    //    }
+    //    printf("입력한 수의 합계 : %d\n", sum);
+    //    printf("입력한 수의 평균 : %d\n", sum / 10);
+
+    return 0;
+}
