5a978e73213a3a42908ede7cede2baa2203a86ac
조병진 <bjcho@jobyeongjin-ui-MacBookAir.local>
2022-04-20 12:23:41 +0900

    배열 추가

---
+++ Codeing class/배열/day2.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  codeup
+//
+//  Created by 조병진 on 2022/04/03.
+//
+
+//배열
+#include <stdio.h>
+
+int main() {
+    //    int num[11], out1 = 0, out2 = 10000;
+    //
+    //    for (int i = 0; i < 10; i++){
+    //        scanf("%d", &num[i]);
+    //    }
+    //
+    //    for (int j = 0; j < 10; j++){
+    //        if (num[j] > 0 && num[j] < 100){
+    //            if(num[j] > out1){
+    //                out1 = num[j];
+    //            }
+    //        }
+    //        else if (num[j] > 99 && num[j] < 10000){
+    //            if(num[j] < out2){
+    //                out2 = num[j];
+    //            }
+    //        }
+    //    }
+    //
+    //    printf("%d %d\n", out1 == 0 ? 100 : out1 , out2 == 10000 ? 100 : out2);
+
+    return 0;
+}
