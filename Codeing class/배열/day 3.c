3e8360f1f7ca78581e0caef9366250e8102bedeb
조병진 <bjcho@jobyeongjin-ui-MacBookAir.local>
2022-04-22 11:34:52 +0900

    3일차 추가

---
+++ Codeing class/배열/day 3.c
@@ -0,0 +1,64 @@
+//
+//  main.c
+//  codeup
+//
+//  Created by 조병진 on 2022/04/03.
+//
+
+//배열
+#include <stdio.h>
+
+int main() {
+    //    char str[1001];
+    //
+    //    scanf("%s", str);
+    //
+    //    for (int c = 0; str[c] != '\0'; c++){
+    //        if (str[c] >= 65 && str[c] <= 90){
+    //            str[c] += 32;
+    //        }
+    //        else if(str[c] >= 97 && str[c] <= 122){
+    //            str[c] -= 32;
+    //        }
+    //    }
+    //
+    //    printf("%s\n", str);
+        
+    //    int num[11], mov;
+    //    for (int i = 0; i < 10; i++){
+    //        scanf("%d", &num[i]);
+    //    }
+    //
+    //    for (int m = 0; m < 10; m++){
+    //        for (int c = 1; c <= 9 - m; c++){
+    //            if (num[m] < num [m + c]){
+    //                mov = num[m];
+    //                num[m] = num[m + c];
+    //                num[m + c] = mov;
+    //            }
+    //
+    //        }
+    //    }
+    //    for (int j = 0; j < 10; j++){
+    //        printf("%d ", num[j]);
+    //    }
+        
+    //    int num[11], sum = 0;
+    //    float avg = 0;
+    //
+    //    for (int i = 0; i < 10; i++){
+    //        scanf("%d", &num[i]);
+    //    }
+    //
+    //    for (int c = 0; c < 10 ; c++){
+    //        if (c % 2){
+    //            sum += num[c];
+    //        }
+    //        else{
+    //            avg += num[c];
+    //        }
+    //    }
+    //    printf("sum : %d\navg : %.1f\n", sum, avg / 5);
+
+    return 0;
+}
