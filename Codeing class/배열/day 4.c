67c6fee94f4b91cc10f0f8a8802934551180c55c
조병진 <bjcho@jobyeongjin-ui-MacBookAir.local>
2022-05-10 16:27:35 +0900

    add day4

---
+++ Codeing class/배열/day 4.c
@@ -0,0 +1,45 @@
+//
+//  main.c
+//  codeup
+//
+//  Created by 조병진 on 2022/04/03.
+//
+
+//배열
+#include <stdio.h>
+
+int main() {
+    //    int a, b, c, out = 0;
+    //
+    //        scanf("%d %d %d", &a, &b, &c);
+    //
+    //        if((c < a && a < b) || (b < a && a < c)){
+    //            out = a;
+    //        }
+    //        else if((a < b && b < c) || (c < b && b < a)){
+    //            out = b;
+    //        }
+    //        else if((b < c && c < a) || (a < c && c < b)){
+    //            out = c;
+    //        }
+    //
+    //        printf("%d\n", out);
+        
+    //    char list[4][6];
+    //
+    //    for (int i = 0; i < 3; i++){
+    //        for (int j = 0; j < 5; j++){
+    //            scanf(" %c", &list[i][j]);
+    //        }
+    //    }
+    //
+    //    for (int i = 0; i < 3; i++){
+    //        for (int j = 0; j < 5; j++){
+    //            list[i][j] += 32;
+    //            printf("%c ", list[i][j]);
+    //        }
+    //        printf("\n");
+    //    }
+
+    return 0;
+}
